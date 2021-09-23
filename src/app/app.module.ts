140951f794ec2586b0ab2132f866696171e524fc
helua <helua@users.noreply.github.com>
2021-09-23 18:51:47 +0200

    project empy edited, instafeed installed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-// import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,15 +10,13 @@ import { NavComponent } from './nav/nav.component';
 import { InfoComponent } from './info/info.component';
 import { GaleriaComponent } from './galeria/galeria.component';
 import { NavMobileComponent } from './nav-mobile/nav-mobile.component';
+import { KontaktComponent } from './kontakt/kontakt.component';
+import { CennikComponent } from './cennik/cennik.component';
 
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
-import { KontaktComponent } from './kontakt/kontakt.component';
-import { CennikComponent } from './cennik/cennik.component';
-
-
 @NgModule({
   declarations: [
     AppComponent,
