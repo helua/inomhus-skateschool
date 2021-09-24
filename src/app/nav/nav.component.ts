dfd55b5f6668e5e99a55c9f2f77eeb1e58522f77
helua <helua@users.noreply.github.com>
2021-09-25 00:04:09 +0200

    info nav menu

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-nav',
@@ -7,9 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
+  // @ViewChild('stickyMenu') menuElement: ElementRef;
+
   constructor() { }
 
   ngOnInit(): void {
   }
+  @HostListener('window:scroll', ['$event'])
+    handleScroll(){
+      const windowScroll = window.pageYOffset;
+      const menu = document.getElementById("nav-main") as HTMLElement;
+      const cont = document.getElementById("nav-main-container") as HTMLElement;
+      const logo = document.getElementById('logo') as HTMLElement;
+
+
+      if(windowScroll >= 160){
+        menu.style.top = "0";
+        cont.style.height = "100px";
+        logo.style.transform = "scale(.8)";
+      } else {
+        menu.style.top = "2%";
+        cont.style.height = "calc(4% + 100px)";
+        logo.style.transform = "scale(1)";
+
+
+      }
+    }
 
 }
